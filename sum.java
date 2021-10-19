1e2b56c30570a3e2adad6fd633881acb8a5c9aed
ashutosh pandey <pandeyashutosh771@gmail.com>
2021-10-19 12:17:24 +0530

    initial project

---
+++ sum.java
@@ -0,0 +1,12 @@
+public class sum {
+
+	public static void main(String[] args) {
+	
+		        int a=4;
+		        int b=5;
+		        int sum= a+b;
+		        System.out.print("Sum of two element"+sum);
+		    
+	}
+
+}
